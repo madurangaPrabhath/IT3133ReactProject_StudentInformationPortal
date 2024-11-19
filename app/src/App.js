b5b25359f011b3486d2160989deacbacb444e2e8
madurangaPrabhath <139701327+madurangaPrabhath@users.noreply.github.com>
2024-11-19 09:33:47 +0000

    Create function to change font size

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,20 @@ import "./assets/css/compo.css";
 import { students } from "./data/StudentsDb";
 
 function App() {
+  const [fontSize, setFontSize] = useState("16px");
+  
+  const changeFontSize = (size) => {
+    if (size === "small") {
+      setFontSize("16px");
+    } else if (size === "medium") {
+      setFontSize("20px");
+    } else if (size === "large") {
+      setFontSize("24px");
+    } else {
+      setFontSize("16px");
+    }
+  };
+  
   return (
     <div className="App">
       
